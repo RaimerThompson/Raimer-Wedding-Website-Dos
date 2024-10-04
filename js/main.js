155e8cf29c165c69d025007bb9b6c01d7785e0ea
Raimer Thompson <rthomp22@gmail.com>
2024-10-03 23:07:47 -0400

    addons

--- js/main.js
+++ js/main.js
@@ -1,65 +1,56 @@
-const carousel = document.querySelector(".carousel");
-firstImg= carousel.querySelectorAll("img")[0];
-arrowIcons = document.querySelectorAll(".wrapper i")
 
-let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
-let firstImgWidth = firstImg.clientWidth + 14; // getting first img width and adding 14 margin value
-
-// updating universal variables on mouse down event
-
-arrowIcons.forEach(icon => {
-  icon.addEventListener("click", () => {
-    let firstImgWidth = firstImg.clientWidth + 14;
-    //if clicked icon is left, reduce width value from the carousel scroll left else add it
-    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-  });
-});
-
-const autoSlide = () => {
-  if (carousel.scrollLeft == (carousel.scrollWidth - carousel.clientWidth)) return;
-
-  positionDiff = Math.abs(positionDiff);
-  let firstImgWidth = firstImg.clientWidth + 14;
-  let valDifference = firstImgWidth - positionDiff;
-
-  if(carousel.scrollLeft > prevScrollLeft){
-    return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+  //image slider
+  const slides = document.querySelector(".home-banner-slide").children;
+  const prev = document.querySelector(".home-prev");
+  const next= document.querySelector(".home-next");
+  let index = 0;
+  
+  prev.addEventListener("click", function(){
+    prevSlide();
+    resetTimer();
+  })
+  
+  next.addEventListener("click", function(){
+    nextSlide();
+    resetTimer();
+  })
+  
+  function prevSlide(){
+    if(index==0){
+      index=slides.length-1;
+    }
+    else{
+      index--;
+    }
+    changeSlide()
   }
-
-  carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-}
-
-const dragStart = (e) =>{
-  isDragStart = true;
-  prevPageX = e.pageX || e.touches[0].pageX
-  prevScrollLeft = carousel.scrollLeft;
-}
-
-const dragging = (e) => {
-  //scrolling img to left accoring to mouse pointer
-  if(!isDragStart) return;
-  e.preventDefault();
-  isDragging = true;
-  carousel.classList.add("dragging")
-  positionDiff = (e.pageX || e.touches[0].pageX)- prevPageX;
-  carousel.scrollLeft = prevScrollLeft - positionDiff;
-}
-
-const dragStop = () =>{
-  isDragStart = false;
-  carousel.classList.remove("dragging");
-
-  if (!isDragging) return;
-  isDragging = false;
-  autoSlide();
-}
-
-carousel.addEventListener("mousedown", dragStart)
-carousel.addEventListener("touchstart", dragStart)
-
-carousel.addEventListener("mousemove", dragging)
-carousel.addEventListener("move", dragging)
-
-carousel.addEventListener("mouseup", dragStop)
-carousel.addEventListener("mouseleave", dragStop)
-carousel.addEventListener("touchend", dragStop)
+  
+  function nextSlide(){
+    if(index == slides.length -1){
+      index = 0;
+    }
+    else {
+      index++
+    }
+    changeSlide();
+  }
+  
+  function changeSlide(){
+      for (let i = 0; i < slides.length; i++){
+        slides[i].classList.remove("slide-active");
+      }
+    slides[index].classList.add("slide-active");
+  }
+  
+  function resetTimer(){
+    clearInterval(timer);
+    timer = setInterval(autoPlay, 4000);
+  }
+  
+  function autoPlay(){
+    nextSlide();
+  }
+  
+  let timer = setInterval(autoPlay, 4000);
+  ;
+  
